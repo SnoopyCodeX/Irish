63c2c2e9e5f02cd8592579482c7bc0c8280b8de6
SnoopyCodeX <extremeclasher@gmail.com>
2022-03-26 17:56:30 +0800

    Initial Commit

---
+++ commands/all.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const getAllCommands = (excludedFiles = []) => {
+  let files = {};
+  
+  fs.readdirSync(__dirname).forEach(function(file) {
+    if (file.match(/\.js$/) !== null && !excludedFiles.includes(file)) {
+      const name = file.replace('.js', '');
+      files[name] = require('./' + file);
+    }
+  });
+  
+  return files;
+}
+
+module.exports = getAllCommands([
+  'all.js'
+]);
---
+++ index.js
@@ -0,0 +1,40 @@
+const BootBot = require("better-bootbot");
+const dotenv = require("dotenv");
+const commands = require("./commands/all");
+dotenv.config();
+
+// Initialize bot
+const bot = new BootBot({
+  accessToken: process.env.ACCESS_TOKEN,
+  verifyToken: process.env.VERIFY_TOKEN,
+  appSecret: process.env.SECRET
+});
+
+// When user starts a convo using "Send to messenger" button
+bot.on("authentication", (payload, chat) => {
+  // Get user profile
+  chat.getUserProfile().then(user => {
+    // Send options to user
+    chat.say({
+      text: `Hi ${user.name}, how may I help you?`,
+      buttons: [
+        {type: "postback", title: "Solve Math", payload: "SOLVE_MATH"},
+        {type: "postback", title: "Search Image", payload: "SEARCH_IMAGE"},
+        {type: "postback", title: "Google Search", payload: "SEARCH_GOOGLE"},
+        {type: "postback", title: "Define Word", payload: "DEFINE_WORD"},
+        {type: "postback", title: "Wiki Search", payload: "SEARCH_WIKI"},
+      ]
+    });
+  });
+});
+/*
+// User clicked "Solve Math" button
+bot.on("postback:SOLVE_MATH", commands.math);
+
+// User clicked "Search Image" button
+bot.on("postback:SEARCH_IMAGE", commands.searchImage);
+
+// User clicked "Search Google" button
+bot.on("postback:SEARCH_GOOGLE", commands.searchGoogle);
+*/
+bot.start();
